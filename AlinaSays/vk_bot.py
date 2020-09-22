82036086d3893971929b926c80f1d4c0c9b36c73
Ъ <nani.l.desu.l.ka@gmail.com>
2020-09-22 16:54:37 +0300

    :bug: Fix: participation bug fix

--- AlinaSays/vk_bot.py
+++ AlinaSays/vk_bot.py
@@ -158,7 +158,7 @@ class VkBot:
     def _participation(self, chat_id):
         output = ""
         for key, value in self._flood[chat_id].get_all().items():
-            output += key + ": " + str(value) + " " + progress(value, self._flood_amount[chat_id]) + '\n'
+            output += str(key) + ": " + str(value) + " " + progress(value, self._flood_amount[chat_id]) + '\n'
         return output
 
     def new_message(self, message, user_id, chat_id):
